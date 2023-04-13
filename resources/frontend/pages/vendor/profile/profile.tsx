313626b2d52defbce0cfbb9acf1ff77bc93b4efd
Tiko <tiko@Tikos-MacBook-Air.local>
2023-04-13 19:45:04 +0400

    vendor profile

--- resources/frontend/pages/vendor/profile/profile.tsx
+++ resources/frontend/pages/vendor/profile/profile.tsx
@@ -14,16 +14,33 @@ const VendorProfile:FC<VendorProfileProps> = ({
     return userData && (
         <div className={cls.profileWrapper}>
             <div className={cls.profile}>
-                <div className={cls.userInfo}>
-                    {Object.keys(userData).length > 0 && <div className={cls.userImage}>
-                        <img src={userData.toString()}/>
-                    </div>}
+                <div className={cls.profileLeft}>
+                    <div className={cls.profileImgWrapper}>
+                        <div className={cls.userInfo}>
+                            {Object.keys(userData).length > 0 && <div className={cls.userImage}>
+                                <img src={userData.toString()}/>
+                            </div>}
+                        </div>
+                    </div>
+                </div>
+                <div className={cls.profileRight}>
+                    <div className={cls.nameBox}>
+                        <h2>{userData.name} {userData.surname}</h2>
+                    </div>
+
+                    <div className={cls.contentBox}>
+                        <p><span>Email</span>  {userData.email}</p>
+                        <p><span>Phone</span>  {userData.phone_number}</p>
+                        <p><span>Birthday</span> {userData.birthday} testBirthday</p>
+                        <p><span>Address</span> {userData.business_address} testAddress</p>
+                    </div>
                 </div>
             </div>
         </div>
     )
 
 }
+
 
 // userData.address
 // userData.birthday
